b5b17795e58894e12c7998278347a521905c571a
komail47 <58760993+komail47@users.noreply.github.com>
2020-01-07 10:09:40 +0300

    ver6

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,20 +1,21 @@
 import React from "react";
 import GaugeChart from "react-gauge-chart";
 
-const Result = ({ score, playAgain,res }) => (
+const Result = ({ score, playAgain, res }) => (
   <div className="score-board">
     <div className="score">
       <GaugeChart
         id="gauge-chart4"
         nrOfLevels={4}
-        
-        colors={["#EA4228", "	#f55f19","#F5CD19", "#5BE12C"]}
+        colors={["#EA4228", "	#f55f19", "#F5CD19", "#5BE12C"]}
         hideText={true}
-        percent={(score/15)}
+        percent={score / 15}
         arcPadding={0}
       />
-   
-      {res} {score}
+
+    
+        {res} {score}
+      
     </div>
     <button className="playBtn" onClick={playAgain}>
       Play again!
--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ class SRP_EHS extends Component {
       questionBank: [],
       score: 0,
       responses: 0,
-      res: String
+      res: String,
+      ans: ""
     };
 
     this.computeAnswer = this.computeAnswer.bind(this);
@@ -28,26 +29,51 @@ class SRP_EHS extends Component {
   };
 
   checkRes = () => {
+    //     let st1 = "Low";
+    //     let st2 = "Low Reliable - Educate yourself on how to improve";
+    //     let st3 = "Reliable - But there is scope of improvement";
+    //     let st4 = "High Reliable - You can count on me";
+
+    //     switch(this.state.score){
+    // case 1:this.state.res = st1;
+    // case 2:this.state.res = st1;
+    // case 3:this.state.res = st1;
+    // case 4:this.state.res = st2;
+    // case 5:this.state.res = st2;
+    // case 6:this.state.res = st2;
+    // case 7:this.state.res = st2;
+    // case 8:this.state.res = st3;
+    // case 9:this.state.res = st3;
+    // case 10:this.state.res = st3;
+    // case 12:this.state.res = st4;
+    // case 13:this.state.res = st4;
+    // case 14:this.state.res = st4;
+    // case 15:this.state.res = st4;
+
+    //     }
     if (this.state.score >= 12) {
-      this.state.res = "high";
-    } else if (this.state.score < 12 & this.state.score >= 8) {
-      this.state.res = "mid";}
-      else if (this.state.score < 12 & this.state.score >= 8) {
-        this.state.res = "mid";
+      this.state.res = "High Reliable - You can count on me";
+    } else if (this.state.score < 12 && this.state.score >= 9) {
+      this.state.res = "Reliable - But there is scope of improvement";
+    } else if (this.state.score < 9 && this.state.score >= 4) {
+      this.state.res = "Low Reliable - Educate yourself on how to improve";
     } else this.state.res = "low";
   };
-
+  //end condition checker
   playAgain = () => {
     this.getQuestion();
     this.setState({
       score: 0,
-      responses: 0
+      responses: 0,
+      res: String,
+      answer: ""
     });
   };
-  async postData() {
+  //end playAgain
+  async postScore() {
     try {
       let result = await fetch(
-        "https://webhook.site/ca3de149-ba73-4906-bc17-91e45f468038",
+        "https://webhook.site/d8a3364c-dfa1-4ddd-a79b-4ce1dc34ead3",
         {
           method: "post",
           mode: "no-cors",
@@ -65,28 +91,56 @@ class SRP_EHS extends Component {
       console.log(e);
     }
   }
+  //end post
+  async postAnswer(ans) {
+    try {
+      let result = await fetch(
+        "https://webhook.site/a2b11cd0-6c73-4efa-bc10-05843eca4344",
+        {
+          method: "post",
+          mode: "no-cors",
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json"
+          },
+          body: JSON.stringify({
+            answer: ans,
+            email: "gmail"
+          })
+        }
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  //end post
+
   computeAnswer = (answer, answerValue) => {
     this.setState({ score: parseInt(this.state.score + answerValue) });
+    this.postAnswer(answer);
+    
     console.log("COMPUTE ANSWER TEXT: " + answer);
-    console.log(answerValue);
+    //console.log(answerValue);
+
     console.log(this.state.score);
+    this.checkRes();
+
     this.setState({
       responses: this.state.responses < 5 ? this.state.responses + 1 : 5
     });
 
-    this.checkRes();
-
     //TODO: Create C# WEB API
+
     //TODO: Connect react to the WEB API
   };
 
   componentDidMount() {
     this.getQuestion();
-    
   }
-componentDidUpdate(){
-  this.postData();
-}
+  componentDidUpdate() {
+   
+  }
+
   render() {
     return (
       <div className="container">
@@ -95,7 +149,7 @@ componentDidUpdate(){
             {" "}
             <img className="bapco_logo" src={Bapco_logo} alt="bapco logo" />
             AM I RELIABLE?
-            <img src={ehs_logo} alt="ehs logo"/>
+            <img src={ehs_logo} alt="ehs logo" />
           </div>
         </div>
         {this.state.questionBank.length > 0 &&
@@ -112,13 +166,15 @@ componentDidUpdate(){
             )
           )}
 
-        {this.state.responses === 5 ? (
-          <Result
-            score={this.state.score}
-            playAgain={this.playAgain}
-            res={this.state.res}
-          />
-        ) : null}
+        {this.state.responses === 5
+          ? this.postScore() && (
+              <Result
+                score={this.state.score}
+                playAgain={this.playAgain}
+                res={this.state.res}
+              />
+            )
+          : null}
       </div>
     );
   }
